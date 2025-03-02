d421a85119ba157f208c2acf642294fe366475c6
Nyxian Code <nekodaisuke.id@gmail.com>
2025-03-03 03:52:01 +0700

    Update __init__.py

--- DeltaMusic/__init__.py
+++ DeltaMusic/__init__.py
@@ -9,42 +9,61 @@ This program is free software: you can redistribute it and can modify
 as you want or you can collabe if you have new ideas.
 """
 
-from DeltaMusic.core.bot import DeltaBot
-from DeltaMusic.core.dir import dirr
-from DeltaMusic.core.cookies import save_cookies
-from DeltaMusic.core.git import git
-from DeltaMusic.core.userbot import Userbot
-from DeltaMusic.misc import dbb, heroku
 
-from .logging import LOGGER
+import asyncio
+import importlib
+from typing import Any
 
-# Directories
-dirr()
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall, GroupCallNotFound
 
-# Check Git Updates
-git()
+import config
+from config import BANNED_USERS
+from DeltaMusic import LOGGER, app, userbot
+from DeltaMusic.core.call import Delta
+from DeltaMusic.misc import sudo
+from DeltaMusic.plugins import ALL_MODULES
+from DeltaMusic.utils.database import get_banned_users, get_gbanned
 
-# Initialize Memory DB
-dbb()
 
-# Heroku APP
-heroku()
+async def init() -> None:
+    # Check for at least one valid Pyrogram string session
+    if all(not getattr(config, f"STRING{i}") for i in range(1, 6)):
+        LOGGER("DeltaMusic").error("Tambahkan sesi string Pyrogram dan coba lagi...")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in await get_gbanned():
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in await get_banned_users():
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for module in ALL_MODULES:
+        importlib.import_module("DeltaMusic.plugins" + module)
+    LOGGER("DeltaMusic.plugins").info("Modul yang Diperlukan Berhasil Diimpor.")
+    await userbot.start()
+    await Delta.start()
+    try:
+        await Delta.stream_call("https://telegra.ph/file/b60b80ccb06f7a48f68b5.mp4")
+    except (NoActiveGroupCall, GroupCallNotFound):
+        LOGGER("DeltaMusic").error(
+            "[ERROR] - \n\nNyalakan obrolan suara grup dan jangan matikan, jika tidak, saya akan berhenti bekerja. Terima kasih."
+        )
+        exit()
+    except:
+        pass
+    await Delta.decorators()
+    LOGGER("DeltaMusic").info("Bot Musik Delta Berhasil Dimulai")
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("DeltaMusic").info("Menghentikan Bot Musik Delta...")
 
-# Cookies
-# save_cookies()
 
-# Bot Client
-app = DeltaBot()
-
-# Assistant Client
-userbot = Userbot()
-
-from .platforms import *
-
-YouTube = YouTubeAPI()
-Carbon = CarbonAPI()
-Spotify = SpotifyAPI()
-Apple = AppleAPI()
-Resso = RessoAPI()
-SoundCloud = SoundAPI()
-Telegram = TeleAPI()
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
+    LOGGER("DeltaMusic").info("Menghentikan Bot Musik")
