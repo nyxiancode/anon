51acce55ddf0b00026bb0f228cdd3f142a8d6836
Nyxian Code <nekodaisuke.id@gmail.com>
2025-03-02 22:07:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
-BOT_USERNAME = getenv("BOT_USERNAME", "xteam_clonebot")
+BOT_USERNAME = getenv("BOT_USERNAME", "NyxiansMusicBot")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI")
@@ -21,7 +21,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 240))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", "-1002385138723"))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002299068113"))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", ))
@@ -34,16 +34,16 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/JIYOXC/Wleowleo",
+    "https://github.com/nyxiancode/anon",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/LullaProject")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/lullagroup")
-SUPPORT_GROUP = getenv("SUPPORT_CHAT", "https://t.me/lullagroup")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NyxianNetwork")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/NyxianSanctuary")
+SUPPORT_GROUP = getenv("SUPPORT_CHAT", "https://t.me/NyxianSanctuary")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -84,21 +84,21 @@ LOG = 2
 LOG_FILE_NAME = "VIPlogs.txt"
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/gw442z.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/j855da.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/8d7b534e34e13316a7dd2.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/j855da.jpg"
 )
-PLAYLIST_IMG_URL ="https://telegra.ph/file/8d7b534e34e13316a7dd2.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL ="https://files.catbox.moe/j855da.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/j855da.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/j855da.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/j855da.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/j855da.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/j855da.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/j855da.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/j855da.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/j855da.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/j855da.jpg"
 
 
 def time_to_seconds(time):
