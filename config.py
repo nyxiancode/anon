715c43e791617d38ac1d4407e813d21ada277d9d
Nyxian Code <nekodaisuke.id@gmail.com>
2025-03-02 22:52:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 240))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", "-1002299068113"))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002131947002"))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", ))
