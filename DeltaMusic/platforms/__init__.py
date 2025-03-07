1e69e8d2f46f46d858a25325f3bda0e9a39d8017
Nyxian Code <nekodaisuke.id@gmail.com>
2025-03-07 20:03:45 +0700

    Update __init__.py

--- DeltaMusic/platforms/__init__.py
+++ DeltaMusic/platforms/__init__.py
@@ -16,5 +16,8 @@ from .Resso import RessoAPI
 from .Soundcloud import SoundAPI
 from .Spotify import SpotifyAPI
 from .Telegram import TeleAPI
-from .Youtube import YouTubeAPI
+from .Youtube import YouTube
 
+class PlaTForms:
+    def __init__(self):
+       self.youtube = YouTube()
